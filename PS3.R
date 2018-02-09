56cd8f84c3bd16dcf05a49fd3e18339582c704f9
Emily Garner <emilyg1995@yahoo.com>
2018-02-09 16:24:52 -0600

    door
    creates object door which randomly generates number between 1 and 3 and defines it as class door

---
+++ PS3.R
@@ -0,0 +1,2 @@
+door = ceiling(runif(1, 0, 3)) ## runif generates a random number between zero and three and ceiling rounds up to the largest integer value ensuring results will be whole numbers one through three
+class(door) = "door" ## defines class of object door as "door"
