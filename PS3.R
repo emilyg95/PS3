2a03d326d035ca1739513d544e8fc4e23f78d02d
Emily Garner <emilyg1995@yahoo.com>
2018-02-09 17:05:11 -0600

    setClass
    creates class door in s4 with one input which must be numeric

--- PS3.R
+++ PS3.R
@@ -9,7 +9,7 @@ PlayGame = function(door){ ## defines existence of PlayGame method for class doo
 }
 
 PlayGame.door = function(door){ ## creates a function using PlayGame method for class door
-  car = ceiling(runif(1, 0, 3)) ## creates object "car" to indicate the number of the winning door. runif generates a random number between 0 and 3 and ceiling rounds up to the largest integer value ensuring the result will be a whole number from 1 through 3
+  car = ceiling(runif(1, 0, 3)) ## creates object "car" to indicate the number of the winning door; runif generates a random number between 0 and 3 and ceiling rounds up to the largest integer value ensuring the result will be a whole number from 1 through 3
   if (door == car){ ## if then statement which prints congratulations if the number door of the input the candidate chose and the number of the winning door are equal
     return("Congratulations! You Win")
   }
@@ -21,11 +21,13 @@ PlayGame.door = function(door){ ## creates a function using PlayGame method for 
 PlayGame(door1) ## test
 
 
-setClass(Class = "door",
+setClass(Class = "door", ## creates a new class "door"
          representation = representation(
-           x = "numeric"
+           x = "numeric" ## indicates 1 input x which must be numeric
          ),
          prototype = prototype(
-           x = c()
+           x = c() ## indicates x will be a list
          )
 )
+
+new("door") ## check
